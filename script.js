de3a6894092cd33d4cfd7f62ef16ba0b5baa16c2
dev-laksh <70800648+dev-laksh@users.noreply.github.com>
2020-11-08 09:16:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('button').click(function(){
+     const name = $('#nameid').val();
+
+    $('.imgchange').attr('src',`https://joeschmoe.io/api/v1/${name}`);
+    });
+   });
